547d84993d17b52d9721e6564f482cdde6f27a1c
Enrique C. <89374388+enrique-co@users.noreply.github.com>
2026-09-11 22:28:00 +0200

    Export ideal power and energy API

--- src/pyextrusion/engineering/__init__.py
+++ src/pyextrusion/engineering/__init__.py
@@ -14,6 +14,15 @@ from .mechanics import (
     specific_pressure_mpa_from_force_mn,
     true_strain_from_extrusion_ratio,
 )
+from .power import (
+    EnergyComparison,
+    compare_specific_energy_kwh_per_tonne,
+    energy_kwh_from_constant_power_kw,
+    hydraulic_power_kw_from_pressure_bar_flow_l_min,
+    ram_power_kw_from_force_mn_speed_mm_s,
+    ram_work_kwh_from_constant_force_mn_stroke_mm,
+    specific_energy_kwh_per_tonne,
+)
 from .press import (
     ForceCapacityCheck,
     HydraulicSystemSpec,
@@ -25,6 +34,7 @@ from .press import (
 
 __all__ = [
     "Confidence",
+    "EnergyComparison",
     "EngineeringResult",
     "EstimateKind",
     "ForceCapacityCheck",
@@ -37,9 +47,15 @@ __all__ = [
     "check_force_capacity",
     "check_press_force_capacity",
     "circular_area_m2_from_diameter_mm",
+    "compare_specific_energy_kwh_per_tonne",
+    "energy_kwh_from_constant_power_kw",
     "force_mn_from_specific_pressure_mpa",
     "hydraulic_force_mn_from_pressure_bar",
+    "hydraulic_power_kw_from_pressure_bar_flow_l_min",
     "hydraulic_pressure_bar_from_force_mn",
+    "ram_power_kw_from_force_mn_speed_mm_s",
+    "ram_work_kwh_from_constant_force_mn_stroke_mm",
+    "specific_energy_kwh_per_tonne",
     "specific_pressure_mpa_from_force_mn",
     "true_strain_from_extrusion_ratio",
 ]
